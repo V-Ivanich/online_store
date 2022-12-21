9fc69346a57fc16c6934a17bcbbe7d37bfb06e84
V-Ivanich <chipai_dom@mail.ru>
2022-12-21 10:18:31 +0300

    directory output

--- src/component/categories/index.jsx
+++ src/component/categories/index.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import { CardItem } from './cardItem'
 import styles from './categories.module.scss'
+import CategoriesList from '../../dataBase/catalog'
 
 export const Categories = () => {
+  const categoriesList = CategoriesList
   return (
     <>
       <Container className={styles.body_categories}>
-        <CardItem />
+        {categoriesList.map((itemCategori) => (
+          <CardItem
+            key={itemCategori.name}
+            itemCategories={itemCategori.name}
+          />
+        ))}
       </Container>
     </>
   )
---
+++ src/dataBase/catalog.js
@@ -0,0 +1,10 @@
+const CategoriesList = [
+  { name: 'Квадроциклы', image: '', links: '' },
+  { name: 'Гидроциклы', image: '', links: '' },
+  { name: 'Катера', image: '', links: '' },
+  { name: 'Снегоходы', image: '', links: '' },
+  { name: 'Вездеходы', image: '', links: '' },
+  { name: 'Двигатели', image: '', links: '' },
+]
+
+export default CategoriesList
