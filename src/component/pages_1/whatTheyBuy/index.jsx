df11b07dfd48097bcb88f9634dd3792a75694d0d
V-Ivanich <chipai_dom@mail.ru>
2023-01-06 23:10:14 +0300

    fix slider

--- src/component/pages_1/whatTheyBuy/index.jsx
+++ src/component/pages_1/whatTheyBuy/index.jsx
@@ -3,22 +3,16 @@ import { Container } from "react-bootstrap";
 import styles from "./what_buy.module.scss";
 
 export const WhatBay = () => {
+    const prevPosition = styles.absolute_btn + " " + styles.prev_position;
     return (
         <Container className="p-0 mt-3">
             <div className={styles.conteiner}>
                 <div className={styles.prev_btn}>
-                    <button className="btn rounded-pill p-1">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="bi bi-chevron-compact-left"
-                            viewBox="0 0 16 16"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M9.224 1.553a.5.5 0 0 1 .223.67L6.56 8l2.888 5.776a.5.5 0 1 1-.894.448l-3-6a.5.5 0 0 1 0-.448l3-6a.5.5 0 0 1 .67-.223z"
-                            />
-                        </svg>
-                    </button>
+                    <div className={prevPosition}>
+                        <button className="btn rounded-pill">
+                            <i className="bi bi-chevron-compact-left sp-3"></i>
+                        </button>
+                    </div>
                 </div>
                 <div className={styles.wrapper}>
                     <div className={styles.slide_set}>
@@ -33,18 +27,11 @@ export const WhatBay = () => {
                     </div>
                 </div>
                 <div className={styles.next_btn}>
-                    <button className="btn rounded-pill p-1">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="bi bi-chevron-compact-right"
-                            viewBox="0 0 16 16"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M6.776 1.553a.5.5 0 0 1 .671.223l3 6a.5.5 0 0 1 0 .448l-3 6a.5.5 0 1 1-.894-.448L9.44 8 6.553 2.224a.5.5 0 0 1 .223-.671z"
-                            />
-                        </svg>
-                    </button>
+                    <div className={styles.absolute_btn}>
+                        <button className="btn rounded-pill">
+                            <i className="bi bi-chevron-compact-right "></i>
+                        </button>
+                    </div>
                 </div>
             </div>
         </Container>
