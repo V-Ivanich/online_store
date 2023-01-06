f532d99437ecf26aa66bf14d397b2eef69f3fdef
V-Ivanich <chipai_dom@mail.ru>
2023-01-06 20:36:56 +0300

    new my slider

--- src/component/pages_1/whatTheyBuy/index.jsx
+++ src/component/pages_1/whatTheyBuy/index.jsx
@@ -1,114 +1,52 @@
 import React from "react";
-import { Container, Carousel, CarouselItem } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import styles from "./what_buy.module.scss";
 
 export const WhatBay = () => {
     return (
-        <>
-            <Container className={styles.body_what}>
-                <Carousel className="carousel" role="listbox">
-                    <CarouselItem className="carousel-item">
-                        <div className="col-md-3">
-                            <div className="card">
-                                <div className="card-img">
-                                    <img
-                                        src="//via.placeholder.com/500x400/31f?text=1"
-                                        className="img-fluid"
-                                    />
-                                </div>
-                                <div className="card-img-overlay">Slide 1</div>
-                            </div>
-                        </div>
-                    </CarouselItem>
-                    <CarouselItem className="carousel-item">
-                        <div className="col-md-3">
-                            <div className="card">
-                                <div className="card-img">
-                                    <img
-                                        src="//via.placeholder.com/500x400/e66?text=2"
-                                        className="img-fluid"
-                                    />
-                                </div>
-                                <div className="card-img-overlay">Slide 2</div>
-                            </div>
-                        </div>
-                    </CarouselItem>
-                    <CarouselItem>
-                        <div className="col-md-3">
-                            <div className="card">
-                                <div className="card-img">
-                                    <img
-                                        src="//via.placeholder.com/500x400/7d2?text=3"
-                                        className="img-fluid"
-                                    />
-                                </div>
-                                <div className="card-img-overlay">Slide 3</div>
-                            </div>
-                        </div>
-                    </CarouselItem>
-                    <CarouselItem className="carousel-item">
-                        <div className="col-md-3">
-                            <div className="card">
-                                <div className="card-img">
-                                    <img
-                                        src="//via.placeholder.com/500x400?text=4"
-                                        className="img-fluid"
-                                    />
-                                </div>
-                                <div className="card-img-overlay">Slide 4</div>
-                            </div>
-                        </div>
-                    </CarouselItem>
-                    <CarouselItem className="carousel-item">
-                        <div className="col-md-3">
-                            <div className="card">
-                                <div className="card-img">
-                                    <img
-                                        src="//via.placeholder.com/500x400/aba?text=5"
-                                        className="img-fluid"
-                                    />
-                                </div>
-                                <div className="card-img-overlay">Slide 5</div>
-                            </div>
-                        </div>
-                    </CarouselItem>
-                    <CarouselItem className="carousel-item">
-                        <div className="col-md-3">
-                            <div className="card">
-                                <div className="card-img">
-                                    <img
-                                        src="//via.placeholder.com/500x400/fc0?text=6"
-                                        className="img-fluid"
-                                    />
-                                </div>
-                                <div className="card-img-overlay">Slide 6</div>
-                            </div>
-                        </div>
-                    </CarouselItem>
-                    <a
-                        className="carousel-control-prev bg-transparent w-aut"
-                        href="#recipeCarousel"
-                        role="button"
-                        data-bs-slide="prev"
-                    >
-                        <span
-                            className="carousel-control-prev-icon"
-                            aria-hidden="true"
-                        ></span>
-                    </a>
-                    <a
-                        className="carousel-control-next bg-transparent w-aut"
-                        href="#recipeCarousel"
-                        role="button"
-                        data-bs-slide="next"
-                    >
-                        <span
-                            className="carousel-control-next-icon"
-                            aria-hidden="true"
-                        ></span>
-                    </a>
-                </Carousel>
-            </Container>
-        </>
+        <Container className="p-0 mt-3">
+            <div className={styles.conteiner}>
+                <div className={styles.prev_btn}>
+                    <button className="btn rounded-pill p-1">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="bi bi-chevron-compact-left"
+                            viewBox="0 0 16 16"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M9.224 1.553a.5.5 0 0 1 .223.67L6.56 8l2.888 5.776a.5.5 0 1 1-.894.448l-3-6a.5.5 0 0 1 0-.448l3-6a.5.5 0 0 1 .67-.223z"
+                            />
+                        </svg>
+                    </button>
+                </div>
+                <div className={styles.wrapper}>
+                    <div className={styles.slide_set}>
+                        <div className={styles.slide_1}>1</div>
+                        <div className={styles.slide_2}>2</div>
+                        <div className={styles.slide_3}>3</div>
+                        <div className={styles.slide_4}>4</div>
+                        <div className={styles.slide_5}>5</div>
+                        <div className={styles.slide_6}>6</div>
+                        <div className={styles.slide_7}>7</div>
+                        <div className={styles.slide_8}>8</div>
+                    </div>
+                </div>
+                <div className={styles.next_btn}>
+                    <button className="btn rounded-pill p-1">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="bi bi-chevron-compact-right"
+                            viewBox="0 0 16 16"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M6.776 1.553a.5.5 0 0 1 .671.223l3 6a.5.5 0 0 1 0 .448l-3 6a.5.5 0 1 1-.894-.448L9.44 8 6.553 2.224a.5.5 0 0 1 .223-.671z"
+                            />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </Container>
     );
 };
