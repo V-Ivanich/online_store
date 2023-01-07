018f6b4b189852579a731879254b1a8123f52d0e
V-Ivanich <chipai_dom@mail.ru>
2023-01-07 22:26:09 +0300

    bibliotek swiper

--- src/component/pages_1/whatTheyBuy/index.jsx
+++ src/component/pages_1/whatTheyBuy/index.jsx
@@ -1,39 +1,61 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { Container } from "react-bootstrap";
-import styles from "./what_buy.module.scss";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import { Pagination, Navigation } from "swiper";
+import "./what_buy.module.css";
+import foto from "../../../image/kvadrocikl.png";
 
 export const WhatBay = () => {
-    const prevPosition = styles.absolute_btn + " " + styles.prev_position;
     return (
-        <Container className="p-0 mt-3">
-            <div className={styles.conteiner}>
-                <div className={styles.prev_btn}>
-                    <div className={prevPosition}>
-                        <button className="btn rounded-pill">
-                            <i className="bi bi-chevron-compact-left sp-3"></i>
-                        </button>
-                    </div>
-                </div>
-                <div className={styles.wrapper}>
-                    <div className={styles.slide_set}>
-                        <div className={styles.slide_1}>1</div>
-                        <div className={styles.slide_2}>2</div>
-                        <div className={styles.slide_3}>3</div>
-                        <div className={styles.slide_4}>4</div>
-                        <div className={styles.slide_5}>5</div>
-                        <div className={styles.slide_6}>6</div>
-                        <div className={styles.slide_7}>7</div>
-                        <div className={styles.slide_8}>8</div>
-                    </div>
-                </div>
-                <div className={styles.next_btn}>
-                    <div className={styles.absolute_btn}>
-                        <button className="btn rounded-pill">
-                            <i className="bi bi-chevron-compact-right "></i>
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </Container>
+        <>
+            <Container className="d-flex mt-2 justify-content-centr">
+                <Swiper
+                    slidesPerView={3}
+                    spaceBetween={30}
+                    slidesPerGroup={3}
+                    loop={true}
+                    loopFillGroupWithBlank={true}
+                    pagination={{
+                        clickable: true
+                    }}
+                    navigation={true}
+                    modules={[Pagination, Navigation]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide className="swiper-slide">
+                        <img src={foto} alt="no" />
+                    </SwiperSlide>
+                    <SwiperSlide className="swiper-slide">
+                        <img src={foto} alt="no" />
+                    </SwiperSlide>
+                    <SwiperSlide className="swiper-slide">
+                        <img src={foto} alt="no" />
+                    </SwiperSlide>
+                    <SwiperSlide className="swiper-slide">
+                        <img src={foto} alt="no" />
+                    </SwiperSlide>
+                    <SwiperSlide className="swiper-slide">
+                        <img src={foto} alt="no" />
+                    </SwiperSlide>
+                    <SwiperSlide className="swiper-slide">
+                        <img src={foto} alt="no" />
+                    </SwiperSlide>
+                    <SwiperSlide className="swiper-slide">
+                        <img src={foto} alt="no" />
+                    </SwiperSlide>
+                    <SwiperSlide className="swiper-slide">
+                        <img src={foto} alt="no" />
+                    </SwiperSlide>
+                    <SwiperSlide className="swiper-slide">
+                        <img src={foto} alt="no" />
+                    </SwiperSlide>
+                </Swiper>
+            </Container>
+        </>
     );
 };
