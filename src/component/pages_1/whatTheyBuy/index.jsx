6cc81f6b4e323b7f7ed11e21cee2bcbeb8e012ca
V-Ivanich <chipai_dom@mail.ru>
2023-01-08 21:17:50 +0300

    setings more

--- src/component/pages_1/whatTheyBuy/index.jsx
+++ src/component/pages_1/whatTheyBuy/index.jsx
@@ -6,73 +6,77 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 import { Pagination, Navigation } from "swiper";
+import { Container } from "react-bootstrap";
 import "./what_buy.module.css";
 import foto from "../../../image/kvadrocikl.png";
 
 export const WhatBay = () => {
     return (
         <>
-            <Swiper
-                slidesPerView={4}
-                spaceBetween={30}
-                slidesPerGroup={3}
-                loop={true}
-                loopFillGroupWithBlank={true}
-                pagination={{
-                    clickable: true
-                }}
-                navigation={true}
-                modules={[Pagination, Navigation]}
-                className="mySwiper"
-            >
-                <div className="swiper-wrapper">
-                    <SwiperSlide className="swiper-slide">
-                        <div className="images">
-                            <img src={foto} alt="no" />
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide className="swiper-slide">
-                        <div className="images">
-                            <img src={foto} alt="no" />
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide className="swiper-slide">
-                        <div className="images">
-                            <img src={foto} alt="no" />
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide className="swiper-slide">
-                        <div className="images">
-                            <img src={foto} alt="no" />
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide className="swiper-slide">
-                        <div className="images">
-                            <img src={foto} alt="no" />
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide className="swiper-slide">
-                        <div className="images">
-                            <img src={foto} alt="no" />
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide className="swiper-slide">
-                        <div className="images">
-                            <img src={foto} alt="no" />
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide className="swiper-slide">
-                        <div className="images">
-                            <img src={foto} alt="no" />
-                        </div>
-                    </SwiperSlide>
-                    <SwiperSlide className="swiper-slide">
-                        <div className="images">
-                            <img src={foto} alt="no" />
-                        </div>
-                    </SwiperSlide>
-                </div>
-            </Swiper>
+            <Container>
+                <Swiper
+                    slidesPerView={4}
+                    spaceBetween={50}
+                    slidesPerGroup={1}
+                    speed={800}
+                    loop={true}
+                    loopFillGroupWithBlank={true}
+                    pagination={{
+                        clickable: true
+                    }}
+                    navigation={true}
+                    modules={[Pagination, Navigation]}
+                    className="mySwiper"
+                >
+                    <div className="swiper-wrapper">
+                        <SwiperSlide className="swiper-slide">
+                            <div className="images">
+                                <img src={foto} alt="no" />
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide className="swiper-slide">
+                            <div className="images">
+                                <img src={foto} alt="no" />
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide className="swiper-slide">
+                            <div className="images">
+                                <img src={foto} alt="no" />
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide className="swiper-slide">
+                            <div className="images">
+                                <img src={foto} alt="no" />
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide className="swiper-slide">
+                            <div className="images">
+                                <img src={foto} alt="no" />
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide className="swiper-slide">
+                            <div className="images">
+                                <img src={foto} alt="no" />
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide className="swiper-slide">
+                            <div className="images">
+                                <img src={foto} alt="no" />
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide className="swiper-slide">
+                            <div className="images">
+                                <img src={foto} alt="no" />
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide className="swiper-slide">
+                            <div className="images">
+                                <img src={foto} alt="no" />
+                            </div>
+                        </SwiperSlide>
+                    </div>
+                </Swiper>
+            </Container>
         </>
     );
 };
