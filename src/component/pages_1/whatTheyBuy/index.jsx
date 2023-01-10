3520ebec95adf22dc3c11848bd017c49aab63411
V-Ivanich <chipai_dom@mail.ru>
2023-01-10 15:10:05 +0300

    fixed swiper

--- src/component/pages_1/whatTheyBuy/index.jsx
+++ src/component/pages_1/whatTheyBuy/index.jsx
@@ -1,82 +1,105 @@
 import React from "react";
+import styles from "./what_buy.module.scss";
 import { Swiper, SwiperSlide } from "swiper/react";
-
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-
-import { Pagination, Navigation } from "swiper";
-import { Container } from "react-bootstrap";
-import "./what_buy.module.css";
+import { Navigation, Thumbs } from "swiper";
 import foto from "../../../image/kvadrocikl.png";
 
 export const WhatBay = () => {
     return (
         <>
-            <Container>
+            <div className={styles.back_col}>
                 <Swiper
                     slidesPerView={4}
-                    spaceBetween={50}
+                    spaceBetween={30}
                     slidesPerGroup={1}
+                    grabCursor={true}
                     speed={800}
                     loop={true}
-                    loopFillGroupWithBlank={true}
-                    pagination={{
-                        clickable: true
-                    }}
                     navigation={true}
-                    modules={[Pagination, Navigation]}
-                    className="mySwiper"
+                    modules={[Thumbs, Navigation]}
+                    className={styles.mySwiper}
                 >
                     <div className="swiper-wrapper">
-                        <SwiperSlide className="swiper-slide">
-                            <div className="images">
+                        <SwiperSlide className={styles.swiper_image}>
+                            <div className={styles.images}>
+                                <img
+                                    className={styles.slide_image}
+                                    src={foto}
+                                    alt="no"
+                                />
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide className={styles.swiper_image}>
+                            <div className={styles.images}>
                                 <img src={foto} alt="no" />
                             </div>
                         </SwiperSlide>
-                        <SwiperSlide className="swiper-slide">
+                        <SwiperSlide className={styles.swiper_image}>
                             <div className="images">
-                                <img src={foto} alt="no" />
+                                <img
+                                    className={styles.slide_image}
+                                    src={foto}
+                                    alt="no"
+                                />
                             </div>
                         </SwiperSlide>
-                        <SwiperSlide className="swiper-slide">
-                            <div className="images">
-                                <img src={foto} alt="no" />
+                        <SwiperSlide>
+                            <div className={styles.images}>
+                                <img
+                                    className={styles.slide_image}
+                                    src={foto}
+                                    alt="no"
+                                />
                             </div>
                         </SwiperSlide>
-                        <SwiperSlide className="swiper-slide">
-                            <div className="images">
-                                <img src={foto} alt="no" />
+                        <SwiperSlide className={styles.swiper_image}>
+                            <div className={styles.images}>
+                                <img
+                                    className={styles.slide_image}
+                                    src={foto}
+                                    alt="no"
+                                />
                             </div>
                         </SwiperSlide>
-                        <SwiperSlide className="swiper-slide">
-                            <div className="images">
-                                <img src={foto} alt="no" />
+                        <SwiperSlide className={styles.swiper_image}>
+                            <div className={styles.images}>
+                                <img
+                                    className={styles.slide_image}
+                                    src={foto}
+                                    alt="no"
+                                />
                             </div>
                         </SwiperSlide>
-                        <SwiperSlide className="swiper-slide">
-                            <div className="images">
-                                <img src={foto} alt="no" />
+                        <SwiperSlide className={styles.swiper_image}>
+                            <div className={styles.images}>
+                                <img
+                                    className={styles.slide_image}
+                                    src={foto}
+                                    alt="no"
+                                />
                             </div>
                         </SwiperSlide>
-                        <SwiperSlide className="swiper-slide">
-                            <div className="images">
-                                <img src={foto} alt="no" />
+                        <SwiperSlide className={styles.swiper_image}>
+                            <div className={styles.images}>
+                                <img
+                                    className={styles.slide_image}
+                                    src={foto}
+                                    alt="no"
+                                />
                             </div>
                         </SwiperSlide>
-                        <SwiperSlide className="swiper-slide">
-                            <div className="images">
-                                <img src={foto} alt="no" />
-                            </div>
-                        </SwiperSlide>
-                        <SwiperSlide className="swiper-slide">
-                            <div className="images">
-                                <img src={foto} alt="no" />
+                        <SwiperSlide className={styles.swiper_image}>
+                            <div className={styles.images}>
+                                <img
+                                    className={styles.slide_image}
+                                    src={foto}
+                                    alt="no"
+                                />
                             </div>
                         </SwiperSlide>
                     </div>
                 </Swiper>
-            </Container>
+            </div>
         </>
     );
 };
